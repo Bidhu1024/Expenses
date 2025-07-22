63e65bbc96825ea25c0a331e6d55b2f77be25c39
Bidhu1024 <bidhu1024av@gmail.com>
2025-07-22 10:12:21 +0530

    db changes

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const connectDB = async()=>{
-    try {
-       
-        const connection = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB is connected at`,connection.connection.host)
-    } catch (error) {
-       console.log(error) 
-        process.exit(1)
-    }
-}
+export const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB is connected at`, connection.connection.host);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
