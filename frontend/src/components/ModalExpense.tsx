493e19593d8bb1588535a2e4c327c6321e77e4be
Bidhu1024 <bidhu1024av@gmail.com>
2025-08-05 13:59:15 +0530

    fix

--- frontend/src/components/ModalExpense.tsx
+++ frontend/src/components/ModalExpense.tsx
@@ -1,5 +1,16 @@
-import { Box, Modal, Typography } from "@mui/material";
-import type { FC } from "react";
+import {
+  Box,
+  Modal,
+  Typography,
+  TextField,
+  TextareaAutosize,
+  Button,
+} from "@mui/material";
+
+import { useState, type FC } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { type SelectChangeEvent } from "@mui/material/Select";
 type Modal = {
   open: boolean;
   setOpen: (val: boolean) => void;
@@ -14,9 +25,17 @@ const style = {
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: 0.6,
 };
 
 const ModalExpense: FC<Modal> = ({ open, setOpen }) => {
+  const [category, setCategory] = useState<string>("No Category");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setCategory(event.target.value as string);
+  };
   return (
     <Modal
       open={open}
@@ -25,12 +44,46 @@ const ModalExpense: FC<Modal> = ({ open, setOpen }) => {
       aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+        <h2>Expense List</h2>
+        <Typography>Title</Typography>
+        <TextField
+          name="title"
+          placeholder="Enter title"
+          size="small"
+          fullWidth
+        />
+        <Typography>Amount</Typography>
+        <TextField
+          name="amount"
+          placeholder="Enter Amount"
+          size="small"
+          fullWidth
+        />
+        <Typography>Category</Typography>
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={category}
+              size="small"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Typography>Date</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker />
+        </LocalizationProvider>
+        <Typography>Notes</Typography>
+        <TextareaAutosize name="title" placeholder="Enter title" />
+        <Button>Cancel</Button>
+        <Button>Save</Button>
       </Box>
     </Modal>
   );
