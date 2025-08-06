87b8ddcfdffd824592868fcb73facee85c3209ee
Bidhu1024 <bidhu1024av@gmail.com>
2025-08-06 10:34:08 +0530

    fixed apis

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { createExpense } from "../controllers/expenseController"
-import { authenticateUser } from "../middleware/auth"
+import { createExpense } from "../controllers/expenseController.js"
+import { authenticateUser } from "../middleware/auth.js"
 
 
 const router = express.Router()
 
 router.post('/create_expense',authenticateUser,createExpense)
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./db.js";
-import dotenv from "dotenv"
-import AuthRoute from "./routes/authRoutes.js"
-import expenseRoutes from "./routes/expenseRoutes.js"
+import dotenv from "dotenv";
+import AuthRoute from "./routes/authRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 dotenv.config();
-app.use(express.json())
-app.use('/api/auth',AuthRoute)
-app.use('/api/expense',expenseRoutes)
+app.use(express.json());
+app.use("/api/auth", AuthRoute);
+app.use("/api/expense", expenseRoutes);
 app.listen(PORT, () => {
   console.log("Backend is working fine at ", PORT);
   connectDB();
