f2a43639b2c9c30e57b9977f580d147066fe6691
Bidhu1024 <bidhu1024av@gmail.com>
2025-08-19 09:57:19 +0530

    Add get api

--- frontend/src/screens/Dashboard/Dashboard.tsx
+++ frontend/src/screens/Dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import Navbar from "../../components/Navbar";
 import { useState } from "react";
 import ModalExpense from "../../components/ModalExpense";
+import ExpensesMain from "../Expenses/ExpensesMain";
 
 const Dashboard = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -9,6 +10,8 @@ const Dashboard = () => {
     <Box width="100%" height={"100vh"} sx={{ background: "#191970" }}>
       <Navbar setOpen={setOpen} />
       <ModalExpense open={open} setOpen={setOpen} />
+
+      <ExpensesMain />
     </Box>
   );
 };
---
+++ frontend/src/screens/Expenses/ExpensesMain.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material"
+
+const ExpensesMain = () => {
+  return (
+    <Box>
+        //API call
+        //map
+        //UI
+    </Box>
+
+  )
+}
+
+export default ExpensesMain
